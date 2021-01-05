65ea2aa5914a978a8d1aa440fed8e530af8b6eef
CruzNadin <cruznadin01@gmail.com>
2021-01-05 11:53:03 +0300

    Version Update

--- index.js
+++ index.js
@@ -152,33 +152,115 @@ if (consoleinput == "new") {
   if (runcode == "ios") {
     oras.start("Building the app.....");
     cmd.run(`npm run ios`, function (err, data, stderr) {
-      if (!stderr) {
-        oras.stop().succeed("Created the application");
-      }
-      if (stderr) {
+      if (err) {
         oras.stop().fail("I can't find the package.json file");
+        return false;
       }
+      oras.stop().succeed("Created the application");
     });
+  } else if (runcode == "clean:ios") {
+    oras.start("Cleaning ios build file.....");
+    cmd.run(
+      `cd ios && xcodebuild clean && cd ..`,
+      function (err, data, stderr) {
+        if (err) {
+          oras.stop().fail("I can't find the package.json file");
+          return false;
+        }
+        oras.stop().succeed("IOS build file cleared");
+      }
+    );
+  } else if (runcode == "ios-device") {
+    var devicename = process.argv[4];
+    oras.start(devicename + " Building the app.....");
+    cmd.run(
+      `npx react-native run-ios --device '${devicename}'`,
+      function (err, data, stderr) {
+        if (err) {
+          oras.stop().fail("I can't find the package.json file");
+          return false;
+        }
+        oras.stop().succeed(devicename + " Created the application");
+      }
+    );
+  } else if (runcode == "ios-release") {
+    oras.start("Ios release mode.....");
+    cmd.run(
+      `npx react-native run-ios --configuration Release`,
+      function (err, data, stderr) {
+        if (err) {
+          oras.stop().fail("I can't find the package.json file");
+          return false;
+        }
+        oras.stop().succeed("Switched to release mode");
+      }
+    );
   } else if (runcode == "android") {
     oras.start("Building the app.....");
     cmd.run(`npm run android`, function (err, data, stderr) {
-      if (!stderr) {
-        oras.stop().succeed("Created the application");
-      }
-      if (stderr) {
+      if (err) {
         oras.stop().fail("I can't find the package.json file");
+        return false;
       }
+      oras.stop().succeed("Created the application");
     });
+  } else if (runcode == "clean:android") {
+    oras.start("Cleaning android build file.....");
+    cmd.run(
+      `cd android && gradlew clean && cd ..`,
+      function (err, data, stderr) {
+        if (err) {
+          oras.stop().fail("I can't find the package.json file");
+          return false;
+        }
+        oras.stop().succeed("Android build file cleared");
+      }
+    );
+  } else if (runcode == "android-release") {
+    oras.start("Android release mode.....");
+    cmd.run(
+      `npx react-native run-android --variant=release`,
+      function (err, data, stderr) {
+        if (err) {
+          oras.stop().fail("I can't find the package.json file");
+          return false;
+        }
+        oras.stop().succeed("Switched to release mode");
+      }
+    );
+  } else if (runcode == "icon") {
+    oras.start("Creating icons.....");
+    cmd.run(
+      `npx react-native set-icon --path ./src/assets/icon_image.png`,
+      function (err, data, stderr) {
+        if (err) {
+          oras.stop().fail("I can't find the package.json file");
+          return false;
+        }
+        oras.stop().succeed("Icons created");
+      }
+    );
+  } else if (runcode == "splash") {
+    oras.start("Creating splash screen.....");
+    cmd.run(
+      `npx react-native set-splash --path ./src/assets/splash_image.png --resize center --background #FDC91B`,
+      function (err, data, stderr) {
+        if (err) {
+          oras.stop().fail("I can't find the package.json file");
+          return false;
+        }
+        oras.stop().succeed("Splash screen created");
+      }
+    );
   } else if (runcode == "pod") {
     if (process.platform == "darwin") {
       oras.start("Loading Pods.....");
       cmd.run(`cd ios && pod install && cd ..`, function (err, data, stderr) {
-        if (!stderr) {
-          oras.stop().succeed("Pods loaded.");
-        }
-        if (stderr) {
+        if (err) {
           oras.stop().fail("I can't find the package.json file");
+          return false;
         }
+        oras.stop().succeed("Pods loaded.");
       });
     } else {
       console.error(
@@ -205,21 +287,45 @@ if (consoleinput == "new") {
       1: ["cn new <app_name>", "To create a new project"],
     },
     {
-      2: ["cn run android", "utility to buy builds for android"],
+      2: ["cn run android", "Utility to buy builds for android"],
     },
     {
-      3: ["cn run ios", "utility to buy builds for ios"],
+      3: ["cn run clean:android", "Cleaning android build file"],
     },
     {
-      4: [
+      4: ["cn run android-release", "Android release mode"],
+    },
+    {
+      5: ["cn run ios", "Utility to buy builds for ios"],
+    },
+    {
+      6: ["cn run clean:ios", "Cleaning ios build file"],
+    },
+    {
+      7: ["cn run ios-device <DeviceName>", "Device building the app"],
+    },
+    {
+      8: ["cn run ios-release", "Ios release mode"],
+    },
+    {
+      9: [
         "cn run pod",
         "Installs the pods required for the project only for ios users",
       ],
     },
     {
-      5: [
+      10: [
         "cn new-services <ServiceName>",
         "You can use this command to create service files.",
+      ],
+    },
+    {
+      11: ["cn run icon", "./src/assets/icon_image.png to creating icons"],
+    },
+    {
+      12: [
+        "cn run splash",
+        "./src/assets/splash_image.png to splash screen only for ios users",
       ],
     }
   );
